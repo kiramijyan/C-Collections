ca7200fd9da5143970b97d89ef2d0c9e16fdd21a
hovo <hkiramijyan@gmail.com>
2020-07-05 22:21:57 +0200

    Arrays First Part

---
+++ Array/Program/Program/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayDemo1();
+        }
+
+        static void ArrayDemo1()
+        {
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wensday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            
+            foreach(string day in daysOfWeek)
+            {
+                Console.WriteLine("Before " + day);
+            }
+
+            daysOfWeek[2] = "Wednesday";
+
+            foreach (string day in daysOfWeek)
+            {
+                Console.WriteLine("After " + day);
+            }
+
+        }
+    }
+}
---
+++ Array/TopTenPops/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pluralsight.BegCShCollections.TopTenPops
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string filePath = @"D:\Bootcamp\C_Sharp\pluralsight\C# Collections\src\Pop by Largest Final.csv";
+			CsvReader reader = new CsvReader(filePath);
+
+			Country[] countries = reader.ReadFirstNCountries(10);
+			
+			foreach (Country country in countries)
+			{
+				Console.WriteLine($"{PopulationFormatter.FormatPopulation (country.Population).PadLeft(15)}: {country.Name}");
+			}
+			Console.ReadLine();
+		}
+	}
+}
