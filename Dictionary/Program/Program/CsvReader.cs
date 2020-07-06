3c47c4664330ebd8bb4135cc2e3284e5c4cbe6fa
hovo <hkiramijyan@gmail.com>
2020-07-06 17:20:07 +0200

    Collections Dictionary

---
+++ Dictionary/Program/Program/CsvReader.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Program
+{
+    class CsvReader
+    {
+        private string _csvFilePath;
+
+        public CsvReader(string csvFilePath)
+        {
+            this._csvFilePath = csvFilePath;
+        }
+
+        public Dictionary<string, Country> ReadAllCountries()
+        {
+            var countries = new Dictionary<string, Country>();
+
+            using (StreamReader sr = new StreamReader(_csvFilePath))
+            {
+                sr.ReadLine();
+                string csvLine = sr.ReadLine();
+                while ((csvLine = sr.ReadLine()) != null)
+                {
+                    Country countryTemp = ReadCountryFromCsvLine(csvLine);
+                    countries.Add(countryTemp.Code, countryTemp);
+                }
+            }
+            return countries;
+        }
+
+        public Country ReadCountryFromCsvLine(string csvLine)
+        {
+            string[] parts = csvLine.Split(',');
+
+            string name;
+            string code;
+            string region;
+            string popText;
+
+            switch (parts.Length)
+            {
+                case 4:
+                    name = parts[0];
+                    code = parts[1];
+                    region = parts[2];
+                    popText = parts[3];
+                    break;
+                case 5:
+                    name = parts[0] + ", " + parts[1];
+                    name = name.Replace("\"", null).Trim();
+                    code = parts[2];
+                    region = parts[3];
+                    popText = parts[4];
+                    break;
+                default:
+                    throw new Exception($"Can't parse country from csvLine: {csvLine}");
+            }
+
+            // int population = int.Parse(parts[3]);
+
+            int.TryParse(popText, out int population);
+            Country country = new Country(name, code, region, population);
+            return country;
+        }
+    }
+}
---
+++ Dictionary/Program/Program/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\Bootcamp\C_Sharp\pluralsight\C# Collections\src\Pop by Largest Final.csv";
+            CsvReader reader = new CsvReader(filePath);
+
+            Dictionary<string, Country> countries = reader.ReadAllCountries();
+
+            Console.WriteLine("Which country code you want to look up? ");
+
+            string userInput = Console.ReadLine();
+
+            bool exist = countries.TryGetValue(userInput, out Country country);
+
+            if (exist)
+            {
+                Console.WriteLine($"{country.Name} has population " +
+                    $"{PopulationFormatter.FormatPopulation(country.Population)}");
+            }
+            else
+            {
+                Console.WriteLine($"No country with {userInput} code was found");
+            }
+        }
+    }
+}
